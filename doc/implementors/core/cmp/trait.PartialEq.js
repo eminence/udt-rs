388f3d353b42fe354912f89c09e67f154f19d26e
Andrew Chin <achin@eminence32.net>
2015-10-04 01:22:45 -0400

    Initial impl for epoll interface

---
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['udt'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a>&lt;<a class='enum' href='udt/enum.UdtStatus.html' title='udt::UdtStatus'>UdtStatus</a>&gt; for <a class='enum' href='udt/enum.UdtStatus.html' title='udt::UdtStatus'>UdtStatus</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='udt/struct.UdtSocket.html' title='udt::UdtSocket'>UdtSocket</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['udt'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='udt/struct.UdtSocket.html' title='udt::UdtSocket'>UdtSocket</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='udt/struct.UdtError.html' title='udt::UdtError'>UdtError</a>",];
+implementors['udt'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='udt/enum.UdtStatus.html' title='udt::UdtStatus'>UdtStatus</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='udt/struct.UdtSocket.html' title='udt::UdtSocket'>UdtSocket</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='udt/struct.UdtError.html' title='udt::UdtError'>UdtError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
