b214a71a233d5efa6c5fec00445907d8ec759fb3
Andrew Chin <achin@eminence32.net>
2015-10-06 23:07:13 -0400

    Add the events arg to epoll_add_usock
    Docs built from 6b937480543881f80be4b184e5f769104ece796f

---
+++ doc/implementors/core/ops/trait.Not.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['udt'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='udt/struct.EpollEvents.html' title='udt::EpollEvents'>EpollEvents</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
