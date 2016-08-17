5c85355171c3ba0a5bf8c102f4160b01c0cc0291
Andrew Chin <achin@eminence32.net>
2016-08-16 20:33:01 -0400

    Update libc and bitflags dependencies
    Rev version dump since a dep was updated
    
    Rework a bunch of platform-specific stuff
    
    Since upgrading to libc 0.2, a lot of needed stuff got removed from the
    libc package, so now using winapi instead.

---
+++ doc/implementors/core/ops/trait.BitXorAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["udt"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitXorAssign.html' title='core::ops::BitXorAssign'>BitXorAssign</a> for <a class='struct' href='udt/struct.EpollEvents.html' title='udt::EpollEvents'>EpollEvents</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
